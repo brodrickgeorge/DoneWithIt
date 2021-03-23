bc69ebd0e9d32d8f7a1f086d7304f542665d473a
brodrickgeorge <brodrick.george@yahoo.com>
2021-03-23 14:41:18 -0500

    changed default screen

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import React from "react";
-// import ListingEditScreen from "./app/screens/ListingEditScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import LoginScreen from "./app/screens/LoginScreen";
+import React, { useEffect, useState } from "react";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <WelcomeScreen />;
 }
